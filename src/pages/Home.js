0ae8cf5ac2bf0564522a04f3a98b593cdf44b3ba
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-07-18 15:41:47 +0200

    log out ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
+import { UserContext } from "../context/userContext";
 
 export default function Home() {
+
+//  J'import mon context pour pouvoir changer le texte de la page home en fonction  du status de l'utilisateur
+  const {currentUser} = useContext(UserContext);
+
   return (
     <div className="conatainer p-5">
       <h1 className="display-3 text-light">
-        Hi, Sign Up or Sign In
+        { currentUser ? "Welcom buddy" : "Hi, Sign Up or Sign In"}
       </h1>
     </div>
   );
